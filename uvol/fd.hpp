42484fcd348b2293ec5eed4db4603a05db3d72c6
Christian Rorvik <christian.rorvik@gmail.com>
2013-07-02 15:42:04 +0100

    Removed unused code

--- uvol/fd.hpp
+++ uvol/fd.hpp
@@ -33,34 +33,6 @@ namespace CqfProject
         double targetDeltaPrice,
         double targetDeltaTime,
         std::vector<OptionContract> contracts);
-    
-    /*
-
-    For multiple expiries, could have a partial evaluation function which returns the whole column of the grid. 
-    The column could be used as payoffs + payoff of newly expired in next partial
-    */
-
-    // payoffFunc(price)
-    // valueFunc(value, price, time)
-    template<typename ValueFunc, typename PayoffFunc>
-    double FiniteDifferenceValue(
-        double maxPrice,
-        double deltaPrice,
-        double maxTime,
-        double deltaTime,
-        double minVol,
-        double maxVol,
-        double rate,
-        ValueFunc valueFunc,
-        PayoffFunc payoffFunc)
-    {
-        typedef std::vector<double> Column;
-
-        Column current;
-
-  
-        return 0.0;
-    }
 }
 
 #endif
