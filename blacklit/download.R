d80396a2140c84c06746eda26f6af2a15035f019
Christian Rorvik <christian.rorvik@gmail.com>
2013-12-17 22:01:05 +0000

    WIP

--- blacklit/download.R
+++ blacklit/download.R
@@ -1,3 +1,8 @@
+# Configuration
+startDate <- '2009-01-01'
+endDate <- '2013-12-01'
+cacheFile <- "cache.RData"
+
 # Read symbol list
 s <- read.csv("Symbols.csv", as.is=T)
 
@@ -5,9 +10,9 @@ s <- read.csv("Symbols.csv", as.is=T)
 markets <- new.env()
 
 # Load cache if exists
-if (file.exists("cache.RData")) {
+if (file.exists(cacheFile)) {
   cat("Loading cached data\n")
-  load("cache.RData", envir=markets)
+  load(cacheFile, envir=markets)
 }
 
 # Download uncached
@@ -16,8 +21,20 @@ getSymbols(
   s$Symbol[!(s$Symbol %in% ls(markets))],
   env=markets,
   auto.assign=T,
-  from='2009-01-01')
+  from=startDate,
+  to=endDate)
 
 # Cache
 cat("Storing data to cache\n")
-save(list=ls(markets), envir=markets, file="cache.RData")
+save(list=ls(markets), envir=markets, file=cacheFile)
+
+
+# 10 year treasury for risk free
+# Other alternatives:
+# ^IRX: 13 week treasury yield
+# ^TNX: 10 year treasury yield
+# ^TYX: 30 year treasury yield
+# ^FVX: 5 year treasury yield
+riskFree <- getSymbols("^TNX", auto.assign=F, from=startDate)
+
+
---
+++ uvol/main.R
@@ -0,0 +1,5 @@
+library(Rcpp)
+
+cxxflags <- paste0("-std=c++0x -Doverride= -I", getwd())
+Sys.setenv("PKG_CXXFLAGS"=cxxflags)
+sourceCpp("finiteDifferencePricer.cpp")
