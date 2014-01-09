806f6fc2108cdeede4e75efed9a995d944449092
Christian Rorvik <christian.rorvik@gmail.com>
2014-01-09 18:12:19 +0000

    Only enable __builtin_assume_aligned for gcc version >= 4.7.0

--- uvol/avx.hpp
+++ uvol/avx.hpp
@@ -9,7 +9,7 @@
 #   include <immintrin.h>
 #endif
 
-#if defined (__GNUC__)
+#if defined (__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 7))
 #   define ASSUME_ALIGNED(p, a) __builtin_assume_aligned(p, a)
 #else
 #   define ASSUME_ALIGNED(p, a) (p)
